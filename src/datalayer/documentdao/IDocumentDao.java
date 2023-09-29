c79554936750c2248404f56348dc043e565bd423
thienphuprogrammer <thienphuprogrammer@gmail.com>
2023-09-29 15:53:52 +0700

    service manager

--- src/datalayer/documentdao/IDocumentDao.java
+++ src/datalayer/documentdao/IDocumentDao.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface IDocumentDao extends IDao<Document> {
     List<Document> getAllDocumentsByProjectId(int projectId) throws Exception;
+
+    Document getDocument(int projectId, int documentId) throws Exception;
 }
